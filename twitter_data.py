a38fc2dd3cf57cbc0c30467da25c340ad8d61531
Rushil <rushil@gala-shah.co.uk>
2018-02-14 13:17:46 +0000

    Updated twitter num of max tweets

--- twitter_data.py
+++ twitter_data.py
@@ -126,7 +126,7 @@ def calc_twitter(team, opp):
     opp_key_a = util.get_key_by_name(opp)
     team_tag = util.get_tag_by_name(team)
     opp_tag = util.get_tag_by_name(opp)
-    max_tweets = 50  # This value could be changed to increase / decrease depending on volume
+    max_tweets = 500  # This value could be changed to increase / decrease depending on volume
     api, auth = get_twitter_api_session()
     print('5. twitter calculations...')
     print(api.rate_limit_status()['resources']['search'])
